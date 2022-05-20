7535e6aed8b74ba792ae30d849b68b0c2d861b58
Sam <sa235@live.ru>
2022-05-20 20:21:03 +0500

    add solution

---
+++ src/DirIndex/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Serilog;
+
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File(@"logs\log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+Log.Information("Directory indexing tool");
